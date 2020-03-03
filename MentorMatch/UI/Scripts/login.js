a6922b8425e003a3db16e21b54a272311f231ace
Raul <jrtapia@asu.edu>
2020-03-03 01:27:29 -0700

    Fixed styling issues to improve consistency throughout pages. Reworked some HTML files to improve visual appeal. Made quick fixes to login.js

--- MentorMatch/UI/Scripts/login.js
+++ MentorMatch/UI/Scripts/login.js
@@ -6,6 +6,9 @@ function LoginButtonHandler() {
     var webMethod = "../../ProjectServices.asmx/ConnectToServer";
     var parameters = "{username: " + JSON.stringify(username) + ", password: " +JSON.stringify(password) + "}";
 
+    if (username == "" || password == "") {
+        alert("Username or Password cannot be empty. Please input valid credentials.")
+    } else {
     //jQuery ajax method
     $.ajax({
         type: "POST",
@@ -19,13 +22,14 @@ function LoginButtonHandler() {
                 alert(responseFromServer);
             } else {
                 ClearForm();
-                window.location.replace("homepage.html");
+                window.location.replace("homePage.html");
             }
         },
         error: function (e) {
             alert("Error: Unable to access the webservice.");
         }
     });
+    } 
 }
 
 function ClearForm() {
