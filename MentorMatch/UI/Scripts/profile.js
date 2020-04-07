dc1891d2f00667a48708a11cf482215708e077bc
Raul <jrtapia@asu.edu>
2020-04-07 15:35:34 -0700

    Profile JS changes

--- MentorMatch/UI/Scripts/profile.js
+++ MentorMatch/UI/Scripts/profile.js
@@ -1,13 +1,25 @@
 ﻿"use strict";
 
 let username = sessionStorage.getItem("username");
-let profilePic = `../Images/${username}.jpg`;
+let profilePic = '../Images/profilePic.png';
+if (http.status != 404) {
+    profilePic = `../Images/${username}.jpg`;
+}
 let fName = sessionStorage.getItem("fName");
 let lName = sessionStorage.getItem("lName");
 let jobTitle = sessionStorage.getItem("jobTitle");
 let email = sessionStorage.getItem("email");
 let bio = sessionStorage.getItem("bio");
 let personality = sessionStorage.getItem("personality");
+
+// Finish all personalities. 
+switch (personality) {
+    case "ENTP": 
+        personality = "Architect";
+        break;
+    default:
+        break;
+}
 
 // Profile page has its information changed by the information pulled from the DB
 function ProfileHandler() {
