cef01e722acf026cfbebdceeaab59b4d6ace55b1
Samantha Young <swyoung2@asu.edu>
2020-04-13 16:06:24 -0700

    personality switches

--- MentorMatch/UI/Scripts/profile.js
+++ MentorMatch/UI/Scripts/profile.js
@@ -9,11 +9,85 @@ let jobTitle = sessionStorage.getItem("jobTitle");
 let email = sessionStorage.getItem("email");
 let bio = sessionStorage.getItem("bio");
 let personality = sessionStorage.getItem("personality");
-
-// Finish all personalities. 
+ 
 switch (personality) {
-    case "ENTP": 
+    case "INTJ": 
         personality = "Architect";
+        break;
+    default:
+        break;
+    case "INTP":
+        personality = "Logician";
+        break;
+    default:
+        break;
+    case "ENTJ":
+        personality = "Commander";
+        break;
+    default:
+        break;
+    case "ENTP":
+        personality = "Debater";
+        break;
+    default:
+        break;
+    case "INFJ":
+        personality = "Advocate";
+        break;
+    default:
+        break;
+    case "INFP":
+        personality = "Mediator";
+        break;
+    default:
+        break;
+    case "ENFJ":
+        personality = "Protagonist";
+        break;
+    default:
+        break;
+    case "ENFP":
+        personality = "Campaigner";
+        break;
+    default:
+        break;
+    case "ISTJ":
+        personality = "Logistician";
+        break;
+    default:
+        break;
+    case "ISFJ":
+        personality = "Defender";
+        break;
+    default:
+        break;
+    case "ESTJ":
+        personality = "Executive";
+        break;
+    default:
+        break;
+    case "ESFJ":
+        personality = "Consul";
+        break;
+    default:
+        break;
+    case "ISTP":
+        personality = "Virtuoso";
+        break;
+    default:
+        break;
+    case "ISFP":
+        personality = "Adventurer";
+        break;
+    default:
+        break;
+    case "ESTP":
+        personality = "Entrepreneur";
+        break;
+    default:
+        break;
+    case "ESFP":
+        personality = "Entertainer";
         break;
     default:
         break;
