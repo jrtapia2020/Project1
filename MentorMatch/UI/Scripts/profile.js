1bce451100d7525bed08ea7cf4963ed759796e43
Raul <jrtapia@asu.edu>
2020-04-20 23:48:41 -0700

    Made changes all around

--- MentorMatch/UI/Scripts/profile.js
+++ MentorMatch/UI/Scripts/profile.js
@@ -235,24 +235,3 @@ function picUploadError(fileName) {
     document.getElementById('newProfilePic').value = null;
     document.getElementById('preview').style.display = "none";
 }
-/*
-document.getElementById('uploader').onsubmit = function () {
-    var formdata = new FormData(); //FormData object
-    var fileInput = document.getElementById('newProfilePic');
-    //Iterating through each files selected in fileInput
-    for (i = 0; i < fileInput.files.length; i++) {
-        //Appending each file to FormData object
-        formdata.append(fileInput.files[i].name, fileInput.files[i]);
-    }
-    //Creating an XMLHttpRequest and sending
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '../../ProjectServices.asmx/Upload');
-    xhr.send(formdata);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            alert(xhr.responseText);
-        }
-    }
-    return false;
-}   
-*/
